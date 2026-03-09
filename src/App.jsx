0800223de8b3ed83d1948c529818482061d1d6f0
Liliana Ramos <a01644969@tec.mx>
2026-03-09 16:55:12 -0600

    Dashboard y estructura

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Formulario de películas</h1>
+        <Dashboard />
+      </div>
+    </>
+  );
+}
+
+export default App;
